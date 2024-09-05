6753d4c76fdeeb89b68965544a577fa9211d87aa
Kizito Daka <kizitodak@gmail.com>
2024-09-05 06:45:55 +0100

    Updated settings.py

--- afridu_reg/settings.py
+++ afridu_reg/settings.py
@@ -25,7 +25,7 @@ STATICFILES_DIRS = [STATIC_DIR,]
 SECRET_KEY = 'django-insecure-or$6auf*@vv-1#eo0xwyacl!=71(k9(#zp-0@+nj^^7p85#i+l'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['afridu-registration.onrender.com',]
 
