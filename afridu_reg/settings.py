94839b8473a83d9c846e5e2294158960ebc9452f
Kizito Daka <kizitodak@gmail.com>
2024-09-15 13:51:30 +0100

    some changes

--- afridu_reg/settings.py
+++ afridu_reg/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'afridu_app.apps.AfriduAppConfig',
+    'whitenoise',
     'crispy_forms',
     'crispy_bootstrap4',
     'django_filters',
@@ -134,6 +135,8 @@ MEDIA_URL = '/media/'
 
 # MEDIA_ROOT = os.path.join(BASE_DIR, 'static/images')
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
+STATICFILES_STORAGE = "whitenoise.storage.CompressedStaticFilesStorage"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
